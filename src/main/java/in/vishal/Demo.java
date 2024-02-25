57a95772914c60e40182417d815b63684fc2f2df
vishal <vishalods225@gmail.com>
2024-02-25 11:11:44 +0530

    first commit

---
+++ src/main/java/in/vishal/Demo.java
@@ -0,0 +1,16 @@
+package in.vishal;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class Demo {
+
+public String encodePwd(String pwd)
+{
+	Encoder encoder = Base64.getEncoder();
+	byte[] encode = encoder.encode(pwd.getBytes());
+	String encodepwd =new String(encode);
+	return encodepwd;
+	
+}
+}
