f0bd3934560dbd456df6f35705d784a9f4319ee0
va-vishal <157092212+va-vishal@users.noreply.github.com>
2024-02-25 23:08:17 +0530

    Update Demo.java

--- src/main/java/in/vishal/Demo.java
+++ src/main/java/in/vishal/Demo.java
@@ -7,6 +7,8 @@ public class Demo {
 
 public String encodePwd(String pwd)
 {
+	int i=11;
+	String vishal="good boy"l
 	Encoder encoder = Base64.getEncoder();
 	byte[] encode = encoder.encode(pwd.getBytes());
 	String encodepwd =new String(encode);
