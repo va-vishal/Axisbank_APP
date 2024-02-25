b5d5bdf7e1f4b91a7360b488ff8e8d4d634ef073
vishal <vishalods225@gmail.com>
2024-02-25 23:10:12 +0530

    vishal

--- src/main/java/in/vishal/Demo.java
+++ src/main/java/in/vishal/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 
 public String encodePwd(String pwd)
 {
+String vishal="abhi is a good b";
 	Encoder encoder = Base64.getEncoder();
 	byte[] encode = encoder.encode(pwd.getBytes());
 	String encodepwd =new String(encode);
