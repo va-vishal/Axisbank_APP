ec24d1122ab4791b957fe022afb04e403e2312d3
vishal <vishalods225@gmail.com>
2024-02-25 23:06:16 +0530

    uypdated

--- src/main/java/in/vishal/Demo.java
+++ src/main/java/in/vishal/Demo.java
@@ -7,7 +7,6 @@ public class Demo {
 
 public String encodePwd(String pwd)
 {
-	int i=10;
 	Encoder encoder = Base64.getEncoder();
 	byte[] encode = encoder.encode(pwd.getBytes());
 	String encodepwd =new String(encode);
